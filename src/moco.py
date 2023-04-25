ca3b24c4b4b3022f66172cc1e11da94ab42d26dc
hahehahaheh123 <125706044+hahehahaheh123@users.noreply.github.com>
2023-04-25 16:14:52 -0400

    Update moco.py

--- src/moco.py
+++ src/moco.py
@@ -10,129 +10,129 @@ logger = logging.getLogger(__name__)
 
 
 class MoCo(nn.Module):
-  def __init__(self, opt):
-    super(MoCo, self).__init__()
-    
-    self.queue_size = opt.queue_size
-    self.momentum = opt.momentum
-    self.temperature = opt.temperature
-    self.label_smoothing = opt.label_smoothing
-    self.norm_doc = opt.norm_doc
-    self.norm_query = opt.norm_query
-    self.moco_train_mode_encoder_k = opt.moco_train_mode_encoder_k  # apply the encoder on keys in train mode
-    
-    retriever, tokenizer = self._load_retriever(
-      opt.retriever_model_id, pooling=opt.pooling, random_init=opt.random_init
-    )
-    
-    self.tokenizer = tokenizer
-    self.encoder_q = retriever
-    self.encoder_k = copy.deepcopy(retriever)
-    
-    for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
-      param_k.data.copy_(param_q.data)
-      param_k.requires_grad = False
+    def __init__(self, opt):
+        super(MoCo, self).__init__()
+
+        self.queue_size = opt.queue_size
+        self.momentum = opt.momentum
+        self.temperature = opt.temperature
+        self.label_smoothing = opt.label_smoothing
+        self.norm_doc = opt.norm_doc
+        self.norm_query = opt.norm_query
+        self.moco_train_mode_encoder_k = opt.moco_train_mode_encoder_k  # apply the encoder on keys in train mode
+
+        retriever, tokenizer = self._load_retriever(
+            opt.retriever_model_id, pooling=opt.pooling, random_init=opt.random_init
+        )
+
+        self.tokenizer = tokenizer
+        self.encoder_q = retriever
+        self.encoder_k = copy.deepcopy(retriever)
+
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data.copy_(param_q.data)
+            param_k.requires_grad = False
       
-    # create the queue
-    self.register_buffer("queue", torch.randn(opt.projection_size, self.queue_size))
-    self.queue = nn.functional.normalize(self.queue, dim=0)
+        # create the queue
+        self.register_buffer("queue", torch.randn(opt.projection_size, self.queue_size))
+        self.queue = nn.functional.normalize(self.queue, dim=0)
     
-    self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
     
-  def _load_retriever(self, model_id, pooling, random_init):
-    cfg = utils.load_hf(transformers.AutoConfig, model_id)
-    tokenizer = utils.load_hf(transformers.AutoTokenizer, model_id)
+    def _load_retriever(self, model_id, pooling, random_init):
+        cfg = utils.load_hf(transformers.AutoConfig, model_id)
+        tokenizer = utils.load_hf(transformers.AutoTokenizer, model_id)
     
-    # **xlm/con choice if
-    model_class = contriever.Contriever
-    
-    if random_init:
-      retriever = model_class(cfg)
-    else:
-      retriever = utils.load_hf(model_class, model_id)
-      
-    if "bert-" in model_id:
-      if tokenizer.bos_token_id is None:
-        tokenizer.bos_token = "[CLS]"
-      if tokenizer.eos_token_id is None:
-        tokenizer.eos_token = "[SEP]"
-        
-    retriever.config.pooling = pooling
-    
-    return retriever, tokenizer
+        # **xlm/con choice if
+        model_class = contriever.Contriever
+
+        if random_init:
+            retriever = model_class(cfg)
+        else:
+            retriever = utils.load_hf(model_class, model_id)
+
+        if "bert-" in model_id:
+            if tokenizer.bos_token_id is None:
+                tokenizer.bos_token = "[CLS]"
+            if tokenizer.eos_token_id is None:
+                tokenizer.eos_token = "[SEP]"
+
+        retriever.config.pooling = pooling
+
+        return retriever, tokenizer
   
-  def get_encoder(self, return_encoder_k=False):
-    if return_encoder_k:
-      return self.encoder_k
-    else:
-      return self.encoder_q
-    
-  def _momentum_update_key_encoder(self):
-      """
-      Update of the key encoder
-      """
-      for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
-        param_k.data = param_k.data * self.momentum + param_q.data * (1.0 - self.momentum)
+    def get_encoder(self, return_encoder_k=False):
+        if return_encoder_k:
+            return self.encoder_k
+        else:
+            return self.encoder_q
+   
+    def _momentum_update_key_encoder(self):
+        """
+        Update of the key encoder
+        """
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data = param_k.data * self.momentum + param_q.data * (1.0 - self.momentum)
         
-  @torch.no_grad()
-  def _dequeue_and_enqueue(self, keys):  # they use a different way to do "last in, first out", still the same
-    # gather keys before updating queue
-    keys = dist_utils.gather_nograd(keys.contiguous())  # returns one concatenated tensor
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):  # they use a different way to do "last in, first out", still the same
+        # gather keys before updating queue
+        keys = dist_utils.gather_nograd(keys.contiguous())  # returns one concatenated tensor
     
-    batch_size = keys.shape[0]
+        batch_size = keys.shape[0]
     
-    ptr = int(self.queue_ptr)
-    assert self.queue_size % batch_size == 0, f"{batch_size}, {self.queue_size}"  # for simplicity
+        ptr = int(self.queue_ptr)
+        assert self.queue_size % batch_size == 0, f"{batch_size}, {self.queue_size}"  # for simplicity
     
-    # replace the keys at ptr (dequeue and enqueue)
-    self.queue[:, ptr : ptr + batch_size] = keys.T
-    ptr = (ptr + batch_size) % self.queue_size  # move pointer
+        # replace the keys at ptr (dequeue and enqueue)
+        self.queue[:, ptr : ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.queue_size  # move pointer
     
-    self.queue_ptr[0] = ptr
+        self.queue_ptr[0] = ptr
     
-  def _compute_logits(self, q, k):
-    l_pos = torch.einsum("nc,nc->n", [q, k]).unsqueeze(-1)  # diagonal only mat mul (2048, 1)
-    l_neg = torch.einsum("nc,ck->nk", [q, self.queue.clone().detach()])  # matrix multiplication (2048, 131072)
-    
-    logits = torch.cat([l_pos, l_neg], dim=1)
-    return logits
+    def _compute_logits(self, q, k):
+        l_pos = torch.einsum("nc,nc->n", [q, k]).unsqueeze(-1)  # diagonal only mat mul (2048, 1)
+        l_neg = torch.einsum("nc,ck->nk", [q, self.queue.clone().detach()])  # matrix multiplication (2048, 131072)
+
+        logits = torch.cat([l_pos, l_neg], dim=1)
+        return logits
   
-  def forward(self, q_tokens, q_mask, k_tokens, k_mask, stats_prefix="", iter_stats={}, **kwargs):
-    bsz = q_tokens.size(0)
+    def forward(self, q_tokens, q_mask, k_tokens, k_mask, stats_prefix="", iter_stats={}, **kwargs):
+        bsz = q_tokens.size(0)
     
-    # compute query features
-    q = self.encoder_q(input_ids=q_tokens, attention_mask=q_mask, normalize=self.norm_query)
+        # compute query features
+        q = self.encoder_q(input_ids=q_tokens, attention_mask=q_mask, normalize=self.norm_query)
     
-    # compute key features
-    with torch.no_grad():  # no gradient to keys
-      self._momentum_update_key_encoder()  # update the key encoder
+        # compute key features
+        with torch.no_grad():  # no gradient to keys
+            self._momentum_update_key_encoder()  # update the key encoder
       
-      if not self.encoder_k.training and not self.moco_train_mode_encoder_k:
-        self.encoder_k.eval()
-        
-      k = self.encoder_k(input_ids=k_tokens, attention_mask=k_mask, normalize=self.norm_doc)
+            if not self.encoder_k.training and not self.moco_train_mode_encoder_k:
+                self.encoder_k.eval()
+
+            k = self.encoder_k(input_ids=k_tokens, attention_mask=k_mask, normalize=self.norm_doc)
       
-    # perform mat mul (dot prods) for logits
-    logits = self._compute_logits(q, k) / self.temperature
+        # perform mat mul (dot prods) for logits
+        logits = self._compute_logits(q, k) / self.temperature
     
-    # labels: positive key indicators
-    labels = torch.zeros(bsz, dtype=torch.long).cuda()  # bc positive is always first
+        # labels: positive key indicators
+        labels = torch.zeros(bsz, dtype=torch.long).cuda()  # bc positive is always first
     
-    loss = torch.nn.functional.cross_entropy(logits, labels, label_smoothing=self.label_smoothing)
+        loss = torch.nn.functional.cross_entropy(logits, labels, label_smoothing=self.label_smoothing)
     
-    self._dequeue_and_enqueue(k)  # moco operates with batch and queue seperately
+        self._dequeue_and_enqueue(k)  # moco operates with batch and queue seperately
     
-    # log stats
-    if len(stats_prefix) > 0:
-      stats_prefix = stats_prefix + "/"
-    iter_stats[f"{stats_prefix}loss"] = (loss.item(), bsz)
-    
-    predicted_idx = torch.argmax(logits, dim=-1)
-    accuracy = 100 * (predicted_idx == labels).float().mean()
-    stdq = torch.std(q, dim=0).mean().item()  # standard dev of embeddings
-    stdk = torch.std(k, dim=0).mean().item()
-    iter_stats[f"{stats_prefix}accuracy"] = (accuracy, bsz)
-    iter_stats[f"{stats_prefix}stdq"] = (stdq, bsz)
-    iter_stats[f"{stats_prefix}stdk"] = (stdk, bsz)
-    
-    return loss, iter_stats
+        # log stats
+        if len(stats_prefix) > 0:
+            stats_prefix = stats_prefix + "/"
+        iter_stats[f"{stats_prefix}loss"] = (loss.item(), bsz)
+
+        predicted_idx = torch.argmax(logits, dim=-1)
+        accuracy = 100 * (predicted_idx == labels).float().mean()
+        stdq = torch.std(q, dim=0).mean().item()
+        stdk = torch.std(k, dim=0).mean().item()
+        iter_stats[f"{stats_prefix}accuracy"] = (accuracy, bsz)
+        iter_stats[f"{stats_prefix}stdq"] = (stdq, bsz)
+        iter_stats[f"{stats_prefix}stdk"] = (stdk, bsz)
+
+        return loss, iter_stats
