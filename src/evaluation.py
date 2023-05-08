399641d13ab01f47dd43e0105a01d4e41b1246a1
hahehahaheh123 <125706044+hahehahaheh123@users.noreply.github.com>
2023-05-07 21:01:29 -0400

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -19,7 +19,7 @@ class SimpleTokenizer(object):
     # Receives text, outputs list of tokens (all instances of contiguous alphanum OR non-whitespace char sequences.)
     ALPHA_NUM = r'[\p{L}\p{N}\p{M}]+'
     NON_WS = r'[^\p{Z}\p{C}]'
-    
+
     def __init__(self):
         """
         Args:
@@ -29,7 +29,7 @@ class SimpleTokenizer(object):
             '(%s)|(%s)' % (self.ALPHA_NUM, self.NON_WS),
             flags=regex.IGNORECASE + regex.UNICODE + regex.MULTILINE
         )
-        
+
     def tokenize(self, text, uncased=False):
         matches = [m for m in self._regexp.finditer(text)]  # iter through text, append to list all match instances
         if uncased:
@@ -45,10 +45,10 @@ QAMatchStats = collections.namedtuple('QAMatchStats', ['top_k_hits', 'questions_
 # returns a new tuple subclass named 'QAMatchStats'
 
 def has_answer(answers, text, tokenizer) -> bool:
-    """ Check if a document contains an answer string."""
+    """Check if a document contains an answer string."""
     text = _normalize(text)
     text = tokenizer.tokenize(text, uncased=True)
-    
+
     for answer in answers:
         answer = _normalize(answer)
         answer = tokenizer.tokenize(answer, uncased=True)
@@ -61,56 +61,56 @@ def has_answer(answers, text, tokenizer) -> bool:
     # if nothing is found, False is returned.
 
 def check_answer(example, tokenizer) -> List[bool]:
-    """ Search through all the top docs to see if they have any of the answers."""
+    """Search through all the top docs to see if they have any of the answers."""
     answers = example['answers']
     ctxs = example['ctxs']
-    
+
     hits = []
-    
+
     for i, doc in enumerate(ctxs):
         text = doc['text']
-        
+
         if text is None:  # cannot find the document for some reason
             logger.warning("no doc in db")
             hits.append(False)
             continue
-            
+
         hits.append(has_answer(answers, text, tokenizer))
-        
+
     return hits
 
 def calculate_matches(data: List, workers_num: int):
     """
-    Evaluates answer presence in the given set of documents. This function is supposed to be used with a large collection of
-    documents and results. It internally forks multiple sub-processes for evaluation and then merges results.
-    :param all_docs: dictionary of the entire documents database. doc_id = (doc_text, title)
-    :param answers: list of answers list. One list per question.
+    Evaluates answers presence in the set of documents. This function is supposed to be used with a large collection of
+    documents and results. It internally forks multiple sub-processes for evaluation and then merges results
+    :param all_docs: dictionary of the entire documents database. doc_id -> (doc_text, title)
+    :param answers: list of answers's list. One list per question
     :param closest_docs: document ids of the top results along with their scores
     :param workers_num: amount of parallel threads to process data
     :param match_type: type of answer matching. Refer to has_answer code for available options
-    :return: matching information typle.
+    :return: matching information tuple.
     top_k_hits - a list where the index is the amount of top documents retrieved and the value is the total amount of
     valid matches across an entire dataset.
     questions_doc_hits - more detailed info with answer matches for every question and every retrieved document
     """
-    
+
     logger.info('Matching answers in top docs...')
-    
+
     tokenizer = SimpleTokenizer()
     get_score_partial = partial(check_answer, tokenizer=tokenizer)
-    
+
     processes = ProcessPool(processes=workers_num)
-    scores = processes.map(get_score_partial, data)  # delegate evaluation to workers_num processes
-    
+    scores = processes.map(get_score_partial, data)
+
     logger.info('Per question validation results len=%d', len(scores))
-    
+
     n_docs = len(data[0]['ctxs'])
     top_k_hits = [0] * n_docs
     for question_hits in scores:
         best_hit = next((i for i, x in enumerate(question_hits) if x), None)
         if best_hit is not None:
             top_k_hits[best_hit:] = [v + 1 for v in top_k_hits[best_hit:]]
-        
+
     return QAMatchStats(top_k_hits, scores)
     
 #################################################
@@ -124,17 +124,17 @@ def _normalize(text):
 def normalize_answer(s):
     def remove_articles(text):
         return regex.sub(r'\b(a|an|the)\b', ' ', text)
-    
+
     def white_space_fix(text):
         return ' '.join(text.split())
-    
+
     def remove_punc(text):
         exclude = set(string.punctuation)
         return ''.join(ch for ch in text if ch not in exclude)
-    
+
     def lower(text):
         return text.lower()
-    
+
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
 def em(prediction, ground_truth):
@@ -166,7 +166,7 @@ def count_inversions(arr):
     inv_count = 0
     lenarr = len(arr)
     for i in range(lenarr):
-        for j in range(i+1, lenarr):
+        for j in range(i + 1, lenarr):
             if (arr[i] > arr[j]):
                 inv_count += 1
     return inv_count
@@ -189,4 +189,4 @@ def eval_batch(scores, inversions, avg_topk, idx_topk):
     for k, s in enumerate(scores):
         s = s.cpu().numpy()
         sorted_idx = np.argsort(-s)
-        score(sorted_idx, inversion, avg_topk, idx_topk
+        score(sorted_idx, inversions, avg_topk, idx_topk)
